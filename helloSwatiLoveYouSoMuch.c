7739bfc0feb04e8ed861bbe643c7d626d84edeb8
sushmithaleela <sushmitha.leela@gmail.com>
2016-05-31 10:15:32 +0530

    try1

---
+++ helloSwatiLoveYouSoMuch.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : helloSwatiLoveYouSoMuch.c
+ Author      : Sushmitha
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Swati is awesome, but idiot, donkey too!"); /* prints Hello World */
+	return EXIT_SUCCESS;
+}
